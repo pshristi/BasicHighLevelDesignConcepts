05ea2cdd059cd198c40fc13a6e1f9d91a5fd2689
shristipathak <shristi.pathak@sirionlabs.com>
2024-11-05 21:44:02 +0530

    first commit

---
+++ src/main/java/com/example/learninghld/LearningHldApplication.java
@@ -0,0 +1,13 @@
+package com.example.learninghld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningHldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningHldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learninghld/LearningHldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learninghld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningHldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
